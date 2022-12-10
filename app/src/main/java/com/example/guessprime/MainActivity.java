d32c77b35eb347f508e8e7f6d790b05f531f8a46
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-12-10 01:57:32 -0500

    added random number code

--- app/src/main/java/com/example/guessprime/MainActivity.java
+++ app/src/main/java/com/example/guessprime/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.guessprime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public static final Random RANDOM = new Random();
+    private TextView number, prime, notPrime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number = findViewById(R.id.number);
+        prime = findViewById(R.id.prime);
+        notPrime = findViewById(R.id.notPrime);
+
+        int random1 = RANDOM.nextInt(100) + 1;
+
+        number.setText(Integer.toString(random1));
+
+
     }
 }
